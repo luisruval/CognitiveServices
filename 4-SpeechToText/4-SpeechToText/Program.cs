419464981f86af255bc13e722e51dbf92131851b
Luis Ruvalcaba <lruval@outlook.com>
2021-07-20 00:11:05 -0500

    Initial commit 4-SpeechToText

--- 4-SpeechToText/4-SpeechToText/Program.cs
+++ 4-SpeechToText/4-SpeechToText/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace _4_SpeechToText
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task FromMic(SpeechConfig speechConfig)
         {
-            Console.WriteLine("Hello World!");
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
         }
+
+        async static Task FromFile(SpeechConfig speechConfig)
+        {
+            using var audioConfig = AudioConfig.FromWavFileInput("PathToFile.wav");
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task FromStream(SpeechConfig speechConfig)
+        {
+            var reader = new BinaryReader(File.OpenRead("PathToFile.wav"));
+            using var audioInputStream = AudioInputStream.CreatePushStream();
+            using var audioConfig = AudioConfig.FromStreamInput(audioInputStream);
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            byte[] readBytes;
+            do
+            {
+                readBytes = reader.ReadBytes(1024);
+                audioInputStream.Write(readBytes, readBytes.Length);
+            } while (readBytes.Length > 0);
+
+            var result = await recognizer.RecognizeOnceAsync();
+
+            switch (result.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task Main(string[] args)
+        {
+            var speechConfig = SpeechConfig.FromSubscription("<paste-your-subscription-key>", "<paste-your-region>");
+            await FromMic(speechConfig);
+            // await FromStream(speechConfig);
+            // await FromFile(speechConfig);
+
+        }
+
+
     }
 }
