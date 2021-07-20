6c06ac257caab71ee663b4693e03d2ce140e9f5f
Luis Ruvalcaba <lruval@outlook.com>
2021-07-20 00:09:28 -0500

    Initial commit 4-SpeechToText

---
+++ 4-SpeechToText/4-SpeechToText/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_SpeechToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
