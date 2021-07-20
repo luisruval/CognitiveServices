af3a59924871074809653bc60480a0d0bf789650
Luis Ruvalcaba <lruval@outlook.com>
2021-07-19 23:51:48 -0500

    Initial commit TextAnalytics

---
+++ 2-TextAnalytics/TextAnalytics/TextAnalytics/Program.cs
@@ -0,0 +1,214 @@
+﻿using Azure;
+using System;
+using System.Globalization;
+using Azure.AI.TextAnalytics;
+
+namespace TextAnalytics
+{
+    class Program
+    {
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("<replace-with-your-text-analytics-key-here>");
+        private static readonly Uri endpoint = new Uri("<replace-with-your-text-analytics-endpoint-here>");
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            // You will implement these methods later in the quickstart.
+            SentimentAnalysisExample(client);
+            SentimentAnalysisWithOpinionMiningExample(client);
+            LanguageDetectionExample(client);
+            EntityRecognitionExample(client);
+            EntityLinkingExample(client);
+            RecognizePIIExample(client);
+            KeyPhraseExtractionExample(client);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void SentimentAnalysisExample(TextAnalyticsClient client)
+        {
+            string inputText = "I had the best day of my life. I wish you were there with me.";
+            DocumentSentiment documentSentiment = client.AnalyzeSentiment(inputText);
+            Console.WriteLine($"Document sentiment: {documentSentiment.Sentiment}\n");
+
+            foreach (var sentence in documentSentiment.Sentences)
+            {
+                Console.WriteLine($"\tText: \"{sentence.Text}\"");
+                Console.WriteLine($"\tSentence sentiment: {sentence.Sentiment}");
+                Console.WriteLine($"\tPositive score: {sentence.ConfidenceScores.Positive:0.00}");
+                Console.WriteLine($"\tNegative score: {sentence.ConfidenceScores.Negative:0.00}");
+                Console.WriteLine($"\tNeutral score: {sentence.ConfidenceScores.Neutral:0.00}\n");
+            }
+        }
+
+        static void SentimentAnalysisWithOpinionMiningExample(TextAnalyticsClient client)
+        {
+            var documents = new List<string>
+            {
+                "The food and service were unacceptable, but the concierge were nice."
+            };
+
+            AnalyzeSentimentResultCollection reviews = client.AnalyzeSentimentBatch(documents, options: new AnalyzeSentimentOptions()
+            {
+                IncludeOpinionMining = true
+            });
+
+            foreach (AnalyzeSentimentResult review in reviews)
+            {
+                Console.WriteLine($"Document sentiment: {review.DocumentSentiment.Sentiment}\n");
+                Console.WriteLine($"\tPositive score: {review.DocumentSentiment.ConfidenceScores.Positive:0.00}");
+                Console.WriteLine($"\tNegative score: {review.DocumentSentiment.ConfidenceScores.Negative:0.00}");
+                Console.WriteLine($"\tNeutral score: {review.DocumentSentiment.ConfidenceScores.Neutral:0.00}\n");
+                foreach (SentenceSentiment sentence in review.DocumentSentiment.Sentences)
+                {
+                    Console.WriteLine($"\tText: \"{sentence.Text}\"");
+                    Console.WriteLine($"\tSentence sentiment: {sentence.Sentiment}");
+                    Console.WriteLine($"\tSentence positive score: {sentence.ConfidenceScores.Positive:0.00}");
+                    Console.WriteLine($"\tSentence negative score: {sentence.ConfidenceScores.Negative:0.00}");
+                    Console.WriteLine($"\tSentence neutral score: {sentence.ConfidenceScores.Neutral:0.00}\n");
+
+                    foreach (SentenceOpinion sentenceOpinion in sentence.Opinions)
+                    {
+                        Console.WriteLine($"\tTarget: {sentenceOpinion.Target.Text}, Value: {sentenceOpinion.Target.Sentiment}");
+                        Console.WriteLine($"\tTarget positive score: {sentenceOpinion.Target.ConfidenceScores.Positive:0.00}");
+                        Console.WriteLine($"\tTarget negative score: {sentenceOpinion.Target.ConfidenceScores.Negative:0.00}");
+                        foreach (AssessmentSentiment assessment in sentenceOpinion.Assessments)
+                        {
+                            Console.WriteLine($"\t\tRelated Assessment: {assessment.Text}, Value: {assessment.Sentiment}");
+                            Console.WriteLine($"\t\tRelated Assessment positive score: {assessment.ConfidenceScores.Positive:0.00}");
+                            Console.WriteLine($"\t\tRelated Assessment negative score: {assessment.ConfidenceScores.Negative:0.00}");
+                        }
+                    }
+                }
+                Console.WriteLine($"\n");
+            }
+        }
+
+        static void LanguageDetectionExample(TextAnalyticsClient client)
+        {
+            DetectedLanguage detectedLanguage = client.DetectLanguage("Ce document est rédigé en Français.");
+            Console.WriteLine("Language:");
+            Console.WriteLine($"\t{detectedLanguage.Name},\tISO-6391: {detectedLanguage.Iso6391Name}\n");
+        }
+
+        static void EntityRecognitionExample(TextAnalyticsClient client)
+        {
+            var response = client.RecognizeEntities("I had a wonderful trip to Seattle last week.");
+            Console.WriteLine("Named Entities:");
+            foreach (var entity in response.Value)
+            {
+                Console.WriteLine($"\tText: {entity.Text},\tCategory: {entity.Category},\tSub-Category: {entity.SubCategory}");
+                Console.WriteLine($"\t\tScore: {entity.ConfidenceScore:F2},\tLength: {entity.Length},\tOffset: {entity.Offset}\n");
+            }
+        }
+
+        static void RecognizePIIExample(TextAnalyticsClient client)
+        {
+            string document = "A developer with SSN 859-98-0987 whose phone number is 800-102-1100 is building tools with our APIs.";
+
+            PiiEntityCollection entities = client.RecognizePiiEntities(document).Value;
+
+            Console.WriteLine($"Redacted Text: {entities.RedactedText}");
+            if (entities.Count > 0)
+            {
+                Console.WriteLine($"Recognized {entities.Count} PII entit{(entities.Count > 1 ? "ies" : "y")}:");
+                foreach (PiiEntity entity in entities)
+                {
+                    Console.WriteLine($"Text: {entity.Text}, Category: {entity.Category}, SubCategory: {entity.SubCategory}, Confidence score: {entity.ConfidenceScore}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No entities were found.");
+            }
+        }
+
+        static void EntityLinkingExample(TextAnalyticsClient client)
+        {
+            var response = client.RecognizeLinkedEntities(
+                "Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, " +
+                "to develop and sell BASIC interpreters for the Altair 8800. " +
+                "During his career at Microsoft, Gates held the positions of chairman, " +
+                "chief executive officer, president and chief software architect, " +
+                "while also being the largest individual shareholder until May 2014.");
+            Console.WriteLine("Linked Entities:");
+            foreach (var entity in response.Value)
+            {
+                Console.WriteLine($"\tName: {entity.Name},\tID: {entity.DataSourceEntityId},\tURL: {entity.Url}\tData Source: {entity.DataSource}");
+                Console.WriteLine("\tMatches:");
+                foreach (var match in entity.Matches)
+                {
+                    Console.WriteLine($"\t\tText: {match.Text}");
+                    Console.WriteLine($"\t\tScore: {match.ConfidenceScore:F2}");
+                    Console.WriteLine($"\t\tLength: {match.Length}");
+                    Console.WriteLine($"\t\tOffset: {match.Offset}\n");
+                }
+            }
+        }
+
+        static void KeyPhraseExtractionExample(TextAnalyticsClient client)
+        {
+            var response = client.ExtractKeyPhrases("My cat might need to see a veterinarian.");
+
+            // Printing key phrases
+            Console.WriteLine("Key phrases:");
+
+            foreach (string keyphrase in response.Value)
+            {
+                Console.WriteLine($"\t{keyphrase}");
+            }
+        }
+
+        static async Task AnalyzeOperationExample(TextAnalyticsClient client)
+        {
+            string inputText = "Microsoft was founded by Bill Gates and Paul Allen.";
+
+            var batchDocuments = new List<string> { inputText };
+
+
+            TextAnalyticsActions actions = new TextAnalyticsActions()
+            {
+                RecognizeEntitiesOptions = new List<RecognizeEntitiesOptions>() { new RecognizeEntitiesOptions() },
+                DisplayName = "Analyze Operation Quick Start Example"
+            };
+
+            AnalyzeBatchActionsOperation operation = await client.StartAnalyzeBatchActionsAsync(batchDocuments, actions);
+
+            await operation.WaitForCompletionAsync();
+
+            Console.WriteLine($"Status: {operation.Status}");
+            Console.WriteLine($"Created On: {operation.CreatedOn}");
+            Console.WriteLine($"Expires On: {operation.ExpiresOn}");
+            Console.WriteLine($"Last modified: {operation.LastModified}");
+            if (!string.IsNullOrEmpty(operation.DisplayName))
+                Console.WriteLine($"Display name: {operation.DisplayName}");
+            Console.WriteLine($"Total actions: {operation.TotalActions}");
+            Console.WriteLine($"  Succeeded actions: {operation.ActionsSucceeded}");
+            Console.WriteLine($"  Failed actions: {operation.ActionsFailed}");
+            Console.WriteLine($"  In progress actions: {operation.ActionsInProgress}");
+
+            await foreach (AnalyzeBatchActionsResult documentsInPage in operation.Value)
+            {
+                RecognizeEntitiesResultCollection entitiesResult = documentsInPage.RecognizeEntitiesActionsResults.FirstOrDefault().Result;
+
+                Console.WriteLine("Recognized Entities");
+
+                foreach (RecognizeEntitiesResult result in entitiesResult)
+                {
+                    Console.WriteLine($"  Recognized the following {result.Entities.Count} entities:");
+
+                    foreach (CategorizedEntity entity in result.Entities)
+                    {
+                        Console.WriteLine($"  Entity: {entity.Text}");
+                        Console.WriteLine($"  Category: {entity.Category}");
+                        Console.WriteLine($"  Offset: {entity.Offset}");
+                        Console.WriteLine($"  Length: {entity.Length}");
+                        Console.WriteLine($"  ConfidenceScore: {entity.ConfidenceScore}");
+                        Console.WriteLine($"  SubCategory: {entity.SubCategory}");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
