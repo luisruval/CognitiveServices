2db742513c71d68ba8b3dc3bda7a6b1241612199
Luis Ruvalcaba <lruval@outlook.com>
2021-07-28 19:28:37 -0500

    Initial commit - Translation Speech

---
+++ 5-Translation/5-Translation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+namespace _5_Translation
+{
+    public class Program
+    {
+        static readonly string SPEECH__SUBSCRIPTION__KEY = "YourSuscriptionKey"; // Inserta tu subscription key
+
+        static readonly string SPEECH__SERVICE__REGION = "southcentralus"; // Cambia si utilizas otra region
+
+        static Task Main() => TranslateSpeechAsync();
+
+        // Traduccion de audios
+        static async Task TranslateSpeechAsync()
+        {
+            var translationConfig =
+                SpeechTranslationConfig.FromSubscription(SPEECH__SUBSCRIPTION__KEY, SPEECH__SERVICE__REGION);
+
+            var fromLanguage = "es-MX";
+            var toLanguages = new List<string> { "en", "fr", "de" };
+            translationConfig.SpeechRecognitionLanguage = fromLanguage;
+            toLanguages.ForEach(translationConfig.AddTargetLanguage);
+
+            using var recognizer = new TranslationRecognizer(translationConfig);
+
+            Console.Write($"Say something in '{fromLanguage}' and ");
+            Console.WriteLine($"we'll translate into '{string.Join("', '", toLanguages)}'.\n");
+
+            var result = await recognizer.RecognizeOnceAsync();
+            if (result.Reason == ResultReason.TranslatedSpeech)
+            {
+                Console.WriteLine($"Recognized: \"{result.Text}\":");
+                foreach (var (language, translation) in result.Translations)
+                {
+                    Console.WriteLine($"Translated into '{language}': {translation}");
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CognitiveServices
-Repo del curso de Platzi de Servicios Cognitivos
+Rama 4: Translation
 
-Bienvenido al repo de Servicios Cognitivos de Platzi
+Esta rama está basada en el inicio rápido para utilizar la traducción de voz utilizando C#. Las instrucciones completas las puedes encontrar en https://docs.microsoft.com/es-mx/azure/cognitive-services/speech-service/get-started-speech-translation?tabs=script%2Cwindowsinstall&pivots=programming-language-csharp
 
-URL del curso:
+Considera que como requisitos es necesario contar con una suscripción de Azure para crear un recurso de Speech
