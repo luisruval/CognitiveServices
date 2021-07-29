e6d93687cf1534314f508cc39e2af14a7cccad2f
Luis Ruvalcaba <lruval@outlook.com>
2021-07-28 21:31:42 -0500

    Initial Commit - OCR

---
+++ 8-OCR/8-OCR/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+
+namespace _8_OCR
+{
+    class Program
+    {
+        // Add your Computer Vision subscription key and endpoint
+        static string subscriptionKey = "PASTE_YOUR_COMPUTER_VISION_SUBSCRIPTION_KEY_HERE";
+        static string endpoint =  "PASTE_YOUR_COMPUTER_VISION_ENDPOINT_HERE";
+
+        private const string READ_TEXT_URL_IMAGE = "https://intelligentkioskstore.blob.core.windows.net/visionapi/suggestedphotos/3.png";
+
+
+        static void Main(string[] args)
+        {
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+            // Extract text (OCR) from a URL image using the Read API
+            ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
+        }
+
+        /*
+         * AUTHENTICATE
+         * Creates a Computer Vision client used by each example.
+         */
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        /*
+         * READ FILE - URL 
+         * Extracts text. 
+         */
+        public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("READ FILE FROM URL");
+            Console.WriteLine();
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile);
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+            Console.WriteLine();
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+
+            // Display the found text.
+            Console.WriteLine();
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Console.WriteLine(line.Text);
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }    
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CognitiveServices
-Repo del curso de Platzi de Servicios Cognitivos
+Rama 8: OCR
 
-Bienvenido al repo de Servicios Cognitivos de Platzi
+Esta rama está basada en el inicio rápido para utilizar OCR (reconocimiento óptico de caracteres) utlizando C#. Las instrucciones completas las puedes encontrar en  https://docs.microsoft.com/es-mx/azure/cognitive-services/computer-vision/quickstarts-sdk/client-library?tabs=visual-studio&pivots=programming-language-csharp
 
-URL del curso:
+Considera que como requisitos es necesario contar con una suscripción de Azure para crear un recurso de Computer vision
